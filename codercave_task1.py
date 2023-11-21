b4dde5c59c712713c00ecca983b0f18f2ca4f3e2
velagandulasohan <99021419+velagandulasohan@users.noreply.github.com>
2023-11-21 17:32:09 +0530

    Update codercave_task1.py

--- codercave_task1.py
+++ codercave_task1.py
@@ -44,3 +44,27 @@ elif select==5:
 else:
   print("invalid option")
 
+
+
+#PHASE 1
+#TASK 2
+
+import random
+characters="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*"
+numbers="1234567890"
+all="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890"
+special_chars="!@#$%^&*"
+both="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*"
+alpha="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+length_password=int(input("enter the length of the password:"))
+numbers=input("do you want numbers:")
+special_requires=input("do you want special requires:")
+if numbers=="yes" and special_requires=="yes":
+  a="".join(random.sample(both,length_password))
+elif numbers=="no" and special_requires=="yes":
+  a="".join(random.sample(characters,length_password))
+elif special_requires=="no" and numbers=="yes":
+  a="".join(random.sample(all,length_password))
+elif special_requires=="no" and numbers=="no":
+  a="".join(random.sample(alpha,length_password))
+print(f"your password is{a}")
